31c67f8dbe882a9a7d07f7dd5052e90713086c13
mohamedabdelhameid <mohameedabdelhameed8879@gmail.com>
2024-10-24 07:19:48 +0300

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,50 +1,41 @@
 import { Container } from "react-bootstrap";
-import aboutImg from '../images/R1light.jpg';
+import aboutImg from "../images/R1light.jpg";
 import { Navs } from "../home";
 import CopyRight from "../copyRight";
-import './about.css';
+import "./about.css";
 
-function About(){
-  return(
+function About() {
+  return (
     <>
-    <Container>
-      <Navs />
-      <div className="container div14Grid">
-        <div className="div22imagge bblockk">
-          <img
-            src={aboutImg}
-            alt=""
-            width="100%"
-            className="imgIndiv22imagge"
-            id="imgIndiv22imagge"
-          />
+      <Container>
+        <Navs />
+        <div className="container div14Grid">
+          <div className="div22imagge bblockk">
+            <img
+              src={aboutImg}
+              alt=""
+              width="100%"
+              className="imgIndiv22imagge"
+              id="imgIndiv22imagge"
+            />
+          </div>
+          <div className="content2sec3 bblockk2">
+            <p className="pContent2sec3">
+              I'm
+              <b> mohamed abdel-hameid</b> .I'm in level two at Faculty of
+              Computer and Artificial Intelligence, Sohag University.
+              <br />I am Frontend Developer with proficient knowledge in HTML ,
+              CSS and Javascript , as well as third-party libraries such as
+              bootstrap, and React js.
+            </p>
+          </div>
         </div>
-        <div className="content2sec3 bblockk2">
-          <p className="pContent2sec3">
-            I'm
-            <b> mohamed abdel-hameid</b> from Sohag.
-            <br />
-            I learned HTML, CSS, JAVASCRIPT, BOOTSTRAP and REACT.JS in Front-End
-            deploma in
-            <b>Code Spark</b>
-            company.
-            <br />
-            I'm in level two in 
-            <b>
-              <a
-                href="https://www.facebook.com/profile.php?id=100063863600500&mibextid=ZbWKwL"
-                className="codeSparkCompany"
-                id="FCAI"
-                > faculty in computer and artificial intelligence </a>
-            </b>
-            in sohag university.
-          </p>
-        </div>
-      </div>
-      <CopyRight />
-    </Container>
-  </>
-  )
+        <CopyRight />
+      </Container>
+    </>
+  );
 }
 
 export default About;
+
+// I am Mohamed Abdel-hamied.I'm in level two at  Faculty of Computer and Artificial Intelligence, Sohag University. I am Frontend Developer with proficient knowledge in HTML , CSS and Javascript , as well as third-party libraries such as jQuery, and React js
